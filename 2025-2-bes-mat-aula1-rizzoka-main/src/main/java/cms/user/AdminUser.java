a5f51a1c708aef3c6e6548a6cf0025403d66e173
RizzoKa <kauarizzo06@gmail.com>
2025-08-20 16:49:30 -0300

    Projeto final

--- 2025-2-bes-mat-aula1-rizzoka-main/src/main/java/cms/user/AdminUser.java
+++ 2025-2-bes-mat-aula1-rizzoka-main/src/main/java/cms/user/AdminUser.java
@@ -1,16 +1,9 @@
 package cms.user;
 
-public class AdminUser {
-	private final int id;
-	private final String nome;
-	private final String email;
-	private final String password;
-
+public class AdminUser extends User{
+	
 	public AdminUser(int id, String nome, String email, String password) {
-		this.id = id;
-		this.nome = nome;
-		this.email = email;
-		this.password = password;
+		super(id, nome, email, password);
 	}
 
 	public int getId() {
@@ -24,7 +17,7 @@ public class AdminUser {
 	public String getEmail() {
 		return email;
 	}
-
+	
 	public void deleteContent(int contentId) {
 		/* exclusivo do admin */ }
 }
